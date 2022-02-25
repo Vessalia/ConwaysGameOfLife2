43f541b4c6ffd71ebf4c108c6fd6d5688e106ae2
Vessalia <nathanielples@outlook.com>
2022-02-25 10:38:02 -0600

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,13 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace ConwaysGameOfLife2
+{
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenTK.Windowing.Desktop;
+
+namespace ConwaysGameOfLife2
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game(GameWindowSettings.Default, NativeWindowSettings.Default))
+                game.Run();
+        }
+    }
+}
